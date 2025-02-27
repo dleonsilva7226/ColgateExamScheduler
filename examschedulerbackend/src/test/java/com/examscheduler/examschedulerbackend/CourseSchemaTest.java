2bc8519ad79f2fb0c24de741fe690ede9afc0e70
dleonsilva7226 <dleonsilva7226@gmail.com>
2025-02-27 01:38:02 -0500

    Set up backend and frontend of exam scheduler and have backend connect to postgresql

---
+++ examschedulerbackend/src/test/java/com/examscheduler/examschedulerbackend/CourseSchemaTest.java
@@ -0,0 +1,29 @@
+package com.examscheduler.examschedulerbackend;
+
+import com.examscheduler.examschedulerbackend.model.Course;
+
+public class CourseSchemaTest {
+
+    void whenSavingPerson_thenPersonIsPersistedCorrectly() {
+        Course newCourse = new Course();
+
+//        do testing here
+
+//        long countBefore = .count();
+//        assertEquals(0, countBefore, "No persons should be present before the test");
+
+//        Person person = new Person();
+//        person.setFirstName("John");
+//        person.setLastName("Doe");
+//
+//        Person savedPerson = personRepository.save(person);
+//
+//        assertNotNull(savedPerson, "Saved person should not be null");
+//        assertTrue(savedPerson.getId() > 0, "ID should be greater than 0"); // Check if ID was generated
+//        assertEquals("John", savedPerson.getFirstName(), "First name should be 'John'");
+//        assertEquals("Doe", savedPerson.getLastName(), "Last name should be 'Doe'");
+
+    }
+
+
+}
