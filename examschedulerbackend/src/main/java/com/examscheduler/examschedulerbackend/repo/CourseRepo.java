dd3c0d684e5fbfe7ba7809f316847609bed831a5
dleonsilva7226 <dleonsilva7226@gmail.com>
2025-03-03 00:49:42 -0500

    Remove folder_name from repository

--- examschedulerbackend/src/main/java/com/examscheduler/examschedulerbackend/repo/CourseRepo.java
+++
@@ -1,8 +0,0 @@
-package com.examscheduler.examschedulerbackend.repo;
-
-import com.examscheduler.examschedulerbackend.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepo extends JpaRepository<Course, Long>{
-
-}
--- examschedulerbackend/src/main/java/com/examscheduler/examschedulerbackend/repo/ScheduleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.examscheduler.examschedulerbackend.repo;
-
-import com.examscheduler.examschedulerbackend.model.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepo extends JpaRepository<Schedule, Long>{
-
-}
